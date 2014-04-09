a4049160303b058a47cf386465e01a7f79468313
Remi Rampin <remirampin@gmail.com>
2014-04-09 15:16:22 -0400

    Adds 'bdiff' alias for Git
    For branch diff. 'git bdiff master foo' shows the changes made on the
    unmerged branch foo based on master.

--- skel.sh
+++ skel.sh
@@ -36,6 +36,7 @@ cat > .gitconfig <<END
 	serve = !git daemon --reuseaddr --verbose  --base-path=. --export-all ./.git
 	fa = !git fetch --all -p && git submodule foreach git fetch --all -p
 	p = !git merge --ff-only
+	bdiff = !sh -c 'git difftool --dir-diff $(git merge-base $1 $2)..$2' -
 END
 
 
