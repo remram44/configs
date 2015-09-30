a3b3a6538abb906237ff7d7638b05bc905594ca1
Remi Rampin <remirampin@gmail.com>
2015-09-30 10:56:00 -0400

    Set HISTTIMEFORMAT

--- skel.sh
+++ skel.sh
@@ -68,6 +68,20 @@ END
 # editor
 #
 cat <<'END'
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+HISTTIMEFORMAT='%F %T '
+
+
 alias ll='ls -lh'
 alias la='ls -lAh'
 alias ifconfig='/sbin/ifconfig'
