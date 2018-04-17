bf625f6874edfb850b6f4bc31ad77f55dae3c837
Remi Rampin <remirampin@gmail.com>
2018-04-17 14:10:54 -0400

    bash: Store a ton of history

--- skel.sh
+++ skel.sh
@@ -124,8 +124,8 @@ HISTCONTROL=ignoreboth
 shopt -s histappend
 
 # for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
-HISTSIZE=1000
-HISTFILESIZE=2000
+HISTSIZE=1000000
+HISTFILESIZE=2000000
 
 HISTTIMEFORMAT='%F %T '
 
